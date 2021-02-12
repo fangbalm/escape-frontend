4cd0c268bdc9192893f3b823c78cf337045f3bdc
jennifer tracy <jenniferetracy@gmail.com>
2021-02-12 18:27:17 -0500

    nav bar

--- src/script.js
+++ src/script.js
@@ -1,2 +1,28 @@
-const button = document.getElementById('button-container')
-console.log(button)
+const body = document.getElementsByTagName('body')
+console.log(body)
+
+const cartBtn = document.querySelector('#cartBtn')
+
+console.log(cartBtn)
+
+
+
+//**********Event Listeners **********//
+
+cartBtn.addEventListener('click', openNav)
+
+
+
+//*********Logic *******//
+
+function openNav(e) {
+    
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+  }
+  
